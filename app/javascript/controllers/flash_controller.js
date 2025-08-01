f49ccd417c645b1721cba51616d7f0d2d9e22e6a
danfkelly <danielkellydev@gmail.com>
2025-08-01 14:47:49 +1000

    created nicer Devise views, fixed sticky flash messages

---
+++ app/javascript/controllers/flash_controller.js
@@ -0,0 +1,28 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static values = { autoDismiss: Number }
+
+  connect() {
+    if (this.autoDismissValue) {
+      this.timeout = setTimeout(() => {
+        this.dismiss()
+      }, this.autoDismissValue)
+    }
+  }
+
+  disconnect() {
+    if (this.timeout) {
+      clearTimeout(this.timeout)
+    }
+  }
+
+  dismiss() {
+    this.element.style.transition = "opacity 0.3s ease-out"
+    this.element.style.opacity = "0"
+    
+    setTimeout(() => {
+      this.element.remove()
+    }, 300)
+  }
+}
